123774affc9b11852fe02bb87985436860cf1973
Nickzy <767265762@qq.com>
2022-09-26 10:41:41 +0800

    feat: add new branch gh-pages

--- src/views/home/util.ts
+++
@@ -1,9 +0,0 @@
-const ET011 = require('../../assets/images/home/ET011.jpg')
-const FS008AP = require('../../assets/images/home/FS008AP.jpg')
-const LS003AP_WHITE = require('../../assets/images/home/LS003AP-WHITE.jpg')
-
-const imgList = [ET011, FS008AP, LS003AP_WHITE]
-
-export {
-  imgList
-}
